1e96e376e3982592d4f3d74d79853b66008ff315
rinn <2031443150@qq.com>
2024-07-09 16:32:04 +0800

    因为每次make都会自动commit，不容易看观察bug是何时引入的，所以改掉了每次make时的自动commit

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -18,9 +18,7 @@ include $(NEMU_HOME)/scripts/build.mk
 
 include $(NEMU_HOME)/tools/difftest.mk
 
-compile_git:
-	$(call git_commit, "compile NEMU")
-$(BINARY):: compile_git
+
 
 # Some convenient rules
 
@@ -34,11 +32,9 @@ NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
-	$(call git_commit, "run NEMU")
 	$(NEMU_EXEC)
 
 gdb: run-env
-	$(call git_commit, "gdb NEMU")
 	gdb -s $(BINARY) --args $(NEMU_EXEC)
 
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
