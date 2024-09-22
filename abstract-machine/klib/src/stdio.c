3ff79ceadb3d7854483fba58b03339997262e448
rinn <2031443150@qq.com>
2024-09-22 09:43:04 +0800

    实习了printf

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,36 +5,87 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+int printf(const char *fmt, ...) //打印到屏幕
+{
+    va_list args;
+    va_start(args, fmt);
+    
+    while (*fmt) 
+    {  // 遍历整个格式字符串
+        if (*fmt == '%') 
+        {  // 处理格式化输出
+            fmt++;
+            if (*fmt == 'd') 
+            {
+                int i = va_arg(args, int); // 获取第一个整数
+                char *num_str;  // 假设数字不会超过20位
+                num_str = itoa(i);  // 将整数转换为字符串
+                for (char *p = num_str; *p != '\0'; p++) 
+                {
+                    putch(*p);
+                }
+            } 
+            else if (*fmt == 's') 
+            {
+                char *s = va_arg(args, char *);  // 获取字符串参数
+                for (; *s != '\0'; s++) 
+                {
+                    putch(*s);
+                }
+            }
+            else 
+            {
+                putch(*fmt);  // 直接输出非格式化字符
+            }
+        } 
+        else 
+        {
+            putch(*fmt);  // 直接输出普通字符
+        }
+        fmt++;  // 移动到下一个字符
+    }
+    
+    va_end(args);
+    return 0;
 }
+
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
+int sprintf(char *out, const char *fmt, ...) 
+{
   va_list args;
   va_start(args, fmt);
   char *str = out;
   
-  while (*fmt) {
-      if (*fmt == '%') {
+  while (*fmt) 
+  {
+      if (*fmt == '%') 
+      {
           fmt++;
-          if (*fmt == 'd') {
+          if (*fmt == 'd') 
+          {
               int i = va_arg(args, int);
               char *num_str;
               num_str = itoa(i); 
-              for (char *p = num_str; *p != '\0'; p++) {
+              for (char *p = num_str; *p != '\0'; p++) 
+              {
                   *str++ = *p;
               }
-          } else if (*fmt == 's') {
+          } 
+          else if (*fmt == 's') 
+          {
               char *s = va_arg(args, char*);
-              while (*s) {
+              while (*s) 
+              {
                   *str++ = *s++;
               }
           }
-      } else {
+      } 
+      else 
+      {
           *str++ = *fmt;
       }
       fmt++;
