06025674b702255938cf4af8b825354900e16985
rinn <2031443150@qq.com>
2024-09-23 21:11:02 +0800

    我发现我vga初始化的时候，出现了花屏，上面别的进程什么信息都有，甚至能出现我qq的画面，我在init中调用update手动更新就好了

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -89,4 +89,5 @@ void init_vga() {
   add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), NULL);
   IFDEF(CONFIG_VGA_SHOW_SCREEN, init_screen());
   IFDEF(CONFIG_VGA_SHOW_SCREEN, memset(vmem, 0, screen_size()));
+  update_screen();
 }
