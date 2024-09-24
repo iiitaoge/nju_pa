e092d60d5dd16bd0a8f3dd7b004bea3638e51393
rinn <2031443150@qq.com>
2024-09-24 13:01:45 +0800

    初步实现了__am_timer_uptime，不过跑分不太正常，是nemu埋的坑，time细节没理解

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,11 +1,15 @@
 #include <am.h>
 #include <nemu.h>
+// #include <riscv.h>
 
 void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uint32_t time_low = inl(RTC_ADDR);
+  uint32_t time_high = inl(RTC_ADDR + 4);
+  uint64_t time_now = ((uint64_t)time_high << 32) | time_low;
+  uptime->us = time_now;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
