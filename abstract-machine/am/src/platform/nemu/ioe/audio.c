7bd06312edbd0ea6c99f23d40f01d88e0006cfc3
rinn <2031443150@qq.com>
2024-10-29 19:12:49 +0800

    声卡实现的有问题，会导致马里奥运行时设备内存访问越界，应该是memcpy写入的长度超过流缓冲区了，暂时把声卡全部注释掉

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -13,33 +13,33 @@
 #define AUDIO_SBUF_SIZE_ADDR (AUDIO_ADDR + 0x0c)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
-static int count = 0;
-static int len = 0;
+// static int count = 0;
+// static int len = 0;
 
 void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = true;
-  cfg->bufsize = 0x10000; // 流缓冲区大小
+  cfg->present = false;
+  // cfg->bufsize = 0x10000; // 流缓冲区大小
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
-  outl(AUDIO_FREQ_ADDR, ctrl->freq);
-  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
-  outl(AUDIO_SAMPLES_ADDR, ctrl->samples); 
-  // 写入完毕了
-  outl(AUDIO_INIT_ADDR, 1);
+  // outl(AUDIO_FREQ_ADDR, ctrl->freq);
+  // outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  // outl(AUDIO_SAMPLES_ADDR, ctrl->samples); 
+  // // 写入完毕了
+  // outl(AUDIO_INIT_ADDR, 1);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = count;
+  // stat->count = count;
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
-  len = ctl->buf.end - ctl->buf.start;
-  uint32_t* audio = (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR;  // 写入流缓冲区
-  memcpy(audio, ctl->buf.start, len);  // 把音频数据存到这个空间里面 sbuf
-  count = inl(AUDIO_COUNT_ADDR);
-  outl(AUDIO_COUNT_ADDR, count + len); // 必须先强转成 uint32
+  // len = ctl->buf.end - ctl->buf.start;
+  // uint32_t* audio = (uint32_t *)(uintptr_t)AUDIO_SBUF_ADDR;  // 写入流缓冲区
+  // memcpy(audio, ctl->buf.start, len);  // 把音频数据存到这个空间里面 sbuf
+  // count = inl(AUDIO_COUNT_ADDR);
+  // outl(AUDIO_COUNT_ADDR, count + len); // 必须先强转成 uint32
 }
