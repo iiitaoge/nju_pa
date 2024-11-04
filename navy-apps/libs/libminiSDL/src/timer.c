bc9f45a7e3c47b84075dab3b205f668dbc796831
rinn <2031443150@qq.com>
2024-11-04 20:41:28 +0800

    加了一个时钟api

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -1,6 +1,7 @@
 #include <NDL.h>
 #include <sdl-timer.h>
 #include <stdio.h>
+#include <assert.h>
 
 SDL_TimerID SDL_AddTimer(uint32_t interval, SDL_NewTimerCallback callback, void *param) {
   return NULL;
@@ -11,7 +12,7 @@ int SDL_RemoveTimer(SDL_TimerID id) {
 }
 
 uint32_t SDL_GetTicks() {
-  return 0;
+  return NDL_GetTicks();
 }
 
 void SDL_Delay(uint32_t ms) {
